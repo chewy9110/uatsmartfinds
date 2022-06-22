adfc2a44e99befd2ba2f2663d7fef0dce61ca500
yuen thye <yuenthye@hotmail.com>
2022-06-22 12:21:31 +0800

    pagination and removal of buttons for index

--- src/main/java/com/smartfinds/smartfinds/repository/ProductRepository.java
+++ src/main/java/com/smartfinds/smartfinds/repository/ProductRepository.java
@@ -1,8 +1,19 @@
 package com.smartfinds.smartfinds.repository;
 
 import com.smartfinds.smartfinds.repository.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ProductRepository extends CrudRepository<Product, Integer> {
+import java.util.List;
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+
+
 
 }

c408bab19c4fd0788321b38d646517f2c6974293
chewy9110 <chewy910@gmail.com>
2022-06-30 06:45:50 +0800

    prod deploy 2

--- src/main/java/com/smartfinds/smartfinds/repository/WatchlistRepository.java
+++ src/main/java/com/smartfinds/smartfinds/repository/WatchlistRepository.java
@@ -1,16 +1,28 @@
 package com.smartfinds.smartfinds.repository;
 
+import com.smartfinds.smartfinds.repository.entity.Product;
 import com.smartfinds.smartfinds.repository.entity.Watchlist;
-import com.sun.xml.bind.v2.schemagen.xmlschema.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 import javax.persistence.Entity;
 import java.util.Date;
+import java.util.List;
 
 public interface WatchlistRepository extends CrudRepository<Watchlist, Integer> {
     // #{#entityName} resolves to "Watchlist"
 //    @Entity()
 //    public class Watchlist {}
-//   // @Query("SELECT(*) FROM WATCHLIST", nativeQuery=true)
-//    //List<Watchlist> findAll(int watchlistid, int userid, int productid, Date dateUpdated, boolean deleteStatus);
+//    @Query("SELECT(*) FROM WATCHLIST", nativeQuery=true)
+//    List<Watchlist> findAll(int watchlistid, int userid, int productid, Date dateUpdated, boolean deleteStatus);
+
+
+    //            join " + "Product p on w.productid=p.productid")
+    @Query("SELECT w, p FROM Watchlist w join Product p on w.productid=p.productid WHERE w.userid = :userid and w.deleteStatus=false order by w.dateUpdated desc")
+    List<Watchlist> findbyOwnerId(@Param("userid") Integer userid);
+//, w.dateUpdated as wUpdated, p.dateUpdated as pUpdated
+
+    @Query("SELECT count(*), w.productid FROM Watchlist w WHERE w.productid = :productid and w.deleteStatus=false group by w.productid")
+    Integer getCount(@Param("productid") Integer productid);
 }
